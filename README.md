76b750f2a0bfbaf10538b7900feaa260aee40598
Arandjel Sarenac <arandjel.sarenac@bay42.io>
2016-07-20 16:41:12 +0200

    update to 3.0.3 neo4j, expose http and bolt endpoints to host, precise64 -> trusty64

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ The setup script is a simple bash shell script (no chef/puppet/etc). I wanted a 
 
 Running `vagrant up` will download and install:
 
-* Ubuntu 12.04 LTS (Base Box) `precise64 http://files.vagrantup.com/precise64.box`
+* Ubuntu 14.04 LTS (Base Box) `trusty64 http://files.vagrantup.com/trusty64.box`
 
 * [Oracle Java](http://www.java.com/) JRE `1.7.0u45`
 > By downloading Java you acknowledge that you have read and accepted the terms of the [end user license agreement](http://www.oracle.com/technetwork/java/javase/terms/license/)
 
-* [Neo4j](http://www.neo4j.org/) Community Edition `2.2.5`
+* [Neo4j](http://www.neo4j.org/) Community Edition `3.0.3`
 > (A)GPLv3 License<br>
 > Port 7474 is forwarded from the host to the guest for accessing Neo4j from the host machine.
 
@@ -21,19 +21,3 @@ Running `vagrant up` will download and install:
 
 * [VirtualBox](https://www.virtualbox.org/) (version 4.2 or later)
 * [Vagrant](http://www.vagrantup.com/)
-
-## npm
-
-If you would prefer to install using npm:
-
-```
-npm install vagrant-neo4j
-```
-
-The above [requirements](#requirements) still must be installed separately. 
-
-`npm start` and `npm stop` are aliases of `vagrant up` and `vagrant destroy -f` respectively. Though, this is true whether installed via npm or not.
-
-## Windows 8.1
-
-If you are using Windows 8.1, make sure to you are using [VirtualBox 4.3.2](https://www.virtualbox.org/wiki/Downloads) or later. There were compatibility problems in older versions which caused BSOD.
--- setup.sh
+++ setup.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
+sudo sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+echo "[provisioner] installing java..."
+echo -ne '\n' | sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get -qq update
+sudo apt-get --assume-yes -qq install oracle-java8-installer
+sudo apt-get --assume-yes -f -qq install oracle-java8-set-default
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get --assume-yes -f autoremove
 
-echo
-echo "Installing Java..."
-cd /usr/local
-wget -nv -O jre-7u45-linux-x64.gz http://javadl.sun.com/webapps/download/AutoDL?BundleId=81812
-tar -xf jre-7u45-linux-x64.gz
-rm jre-7u45-linux-x64.gz
-ln -s /usr/local/jre1.7.0_45/bin/java /usr/bin/java
+wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add - 
+echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+sudo apt-get -qq update
+sudo apt-get --assume-yes -qq install neo4j
+echo "[provisioner] stopping neo4j..."
+neo4j stop
 
-echo
-echo "Installing Neo4j..."
-cd /etc
-wget -nv http://dist.neo4j.org/neo4j-community-2.2.5-unix.tar.gz
-tar -xf neo4j-community-2.2.5-unix.tar.gz
-rm neo4j-community-2.2.5-unix.tar.gz
-ln -s /etc/neo4j-community-2.2.5/bin/neo4j /usr/bin/neo4j
+echo "NEO4J_ULIMIT_NOFILE=50000" >> neo4j
+sudo cp neo4j /etc/default/
+sudo chown neo4j /etc/default/neo4j
 
-echo
-echo "Updating Neo4j Config..."
-sed -i 's/#org\.neo4j\.server\.webserver\.address=0\.0\.0\.0/org.neo4j.server.webserver.address=0.0.0.0/' /etc/neo4j-community-2.2.5/conf/neo4j-server.properties
-
-echo
-echo "Starting Neo4j..."
-neo4j start
+echo "[provisioner] updating config..."
+sed -i 's/#dbms\.connector\.http\.address=0\.0\.0\.0/dbms.connector.http.address=0.0.0.0/' /etc/neo4j/neo4j.conf
+sed -i 's/#dbms\.connector\.bolt\.address=0\.0\.0\.0/dbms.connector.bolt.address=0.0.0.0/' /etc/neo4j/neo4j.conf
+echo "[provisioner] bolt and http endpoints opened!"
+echo "[provisioner] starting neo4j..."
+sudo service neo4j start
